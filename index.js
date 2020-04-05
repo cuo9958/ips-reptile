e5e35d58243281a9eb6d8c55d637f2942a5114d7
guofangchao <guofangchao@daling.com>
2020-04-05 20:08:07 +0800

    修改入口方法

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-const puppeteer = require("puppeteer");
-const webClient = require("./src/web");
 const apis = require("./src/apis");
 const CronJob = require("cron").CronJob;
 
 async function main() {
-    const browser = await puppeteer.launch({
-        defaultViewport: {
-            width: 1400,
-            height: 720
-        },
-        slowMo: 10
-    });
-    //创建新的页面对象
-    const page = await browser.newPage();
-    const len = webClient.length;
-    if (len > 0) {
-        let curr = 0;
-        // webClient[curr](page, browser);
-        new CronJob("*/10 * * * * *", async function() {
-            try {
-                await webClient[curr](page, browser);
-            } catch (error) {
-                console.log(error);
-            } finally {
-                curr++;
-                if (curr >= len) curr = 0;
-            }
-        }).start();
-    }
-
     let apiIndex = 0;
     new CronJob("*/5 * * * * *", async function() {
         try {
